c8f1fe02d9402a1f5d4f061b1d9e06e1fdf96b7e
victorialeaf <32233962+victorialeaf@users.noreply.github.com>
2017-10-18 15:42:38 -0500

    Initial Commit

---
+++ geoMessenger/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  geoMessenger
+//
+//  Created by Victoria Leaf on 10/18/17.
+//  Copyright © 2017 Victoria Leaf. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
