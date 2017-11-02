1c284720e9e0eb5f9fff1541a56eaf487b2609bf
victorialeaf <32233962+victorialeaf@users.noreply.github.com>
2017-11-01 22:52:49 -0500

    11.1

--- geoMessenger/ViewController.swift
+++ geoMessenger/ViewController.swift
@@ -6,14 +6,46 @@
 //  Copyright © 2017 Victoria Leaf. All rights reserved.
 //
 
+
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class TablesViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var txtLastname: UITextField!
+    
+    @IBOutlet weak var txtFirstname: UITextField!
+    
+    @IBAction func btnAddUser(_ sender: CustomButton) {
+        
+        var ref: DatabaseReference!
+        
+        ref = FIRDatabase.database().reference()
+        
+        let userTable : [String : Any] =
+            ["FirstName": txtFirstname.text!,
+             "LastName": txtLastname.text!,
+             "IsApproved": false]
+        
+        // add to the Firebase JSON node for MyUsers
+        ref.child("MyUsers").childByAutoId().setValue(userTable)
+        
+        // show confirmation alert
+        let ac = UIAlertController(title: "User Saved!", message:"The user information  was saved successfully!", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        
+        // reset controls
+        txtLastname.text = nil
+        txtFirstname.text = nil
+    }
 }
+
+
 
